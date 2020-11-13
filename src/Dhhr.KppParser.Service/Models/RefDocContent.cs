0cb6caabf0e5346b552c8e2d847d795bd6a148e3
kryel <kryel@users.noreply.github.com>
2020-11-13 15:37:41 +0100

    fix: use correct namespace in RefDocContent.Melding

--- src/Dhhr.KppParser.Service/Models/RefDocContent.cs
+++ src/Dhhr.KppParser.Service/Models/RefDocContent.cs
@@ -4,7 +4,7 @@ namespace Dhhr.KppParser.Service.Models
 {
     public partial class RefDocContent
     {
-        [XmlElement(Namespace = "http://www.npr.no/xmlstds/53_1_3_kpp", ElementName = "Melding")]
+        [XmlElement(Namespace = "http://www.npr.no/xmlstds/54_0_1_kpp", ElementName = "Melding")]
         public Melding Melding { get; set; }
     }
 }
