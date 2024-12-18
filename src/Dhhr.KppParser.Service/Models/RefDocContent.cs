087c91c6c82f429ea63dce8b0cacbe2acab923f0
Karen Johanne Schei Vollan <Karen.Johanne.Schei.Vollan@fhi.no>
2024-12-18 14:20:25 +0100

    oppdaterer meldingsversjon 58 på nytt

--- src/Dhhr.KppParser.Service/Models/RefDocContent.cs
+++ src/Dhhr.KppParser.Service/Models/RefDocContent.cs
@@ -4,7 +4,7 @@ namespace Dhhr.KppParser.Service.Models
 {
     public partial class RefDocContent
     {
-        [XmlElement(Namespace = "http://www.npr.no/xmlstds/57_0_1_kpp", ElementName = "Melding")]
+        [XmlElement(Namespace = "http://www.npr.no/xmlstds/58_0_1_kpp", ElementName = "Melding")]
         public Melding Melding { get; set; }
     }
 }
